eed18490157cf471f7b3233480e3d83bbd239afd
DriesN <driesnackaerts90@hotmail.com>
2014-11-19 11:29:52 +0100

    Toevoeging koeling aan systeemidentificatie_1zone

--- systeemidentificatie/systeemidentificatie_1zone.m
+++ systeemidentificatie/systeemidentificatie_1zone.m
@@ -1,22 +1,32 @@
+%search Temperatuur
+signalname = 'Temperatuur';
+signal_temp = find_signal(data,signalname);
+temp = data.signal(signal_temp).data(range);
+
 % search Toperationeel
 signalname = 'Toperationeel';
-signal_temp = find_signal(data,signalname);
-
+signal_temp_ambient = find_signal(data,signalname);
 
 % search Intern (invallende zonne-energie)
 signalname = 'Intern';
 signal_solar = find_signal(data,signalname);
 
-
 %search Verwarming
 signalname = 'Verwarming';
 signal_verw = find_signal(data,signalname);
 
+%search Koeling
+signalname = 'Koeling';
+signal_koel = find_signal(data,signalname);
+
 %calculate average temp in the 3 zones
-temp_average = mean([data.signal(signal_temp(1)).data(range) data.signal(signal_temp(2)).data(range) data.signal(signal_temp(3)).data(range)],2);
+temp_ambient_average = mean([data.signal(signal_temp_ambient(1)).data(range) data.signal(signal_temp_ambient(2)).data(range) data.signal(signal_temp_ambient(3)).data(range)],2);
 
 %calculate average solarenergy in the 3 zones
 solar_average = mean([data.signal(signal_solar(1)).data(range) data.signal(signal_solar(2)).data(range) data.signal(signal_solar(3)).data(range)],2);
 
 %calculate average heating in the 3 zones
 verw_average = mean([data.signal(signal_verw(2)).data(range) data.signal(signal_verw(3)).data(range) data.signal(signal_verw(4)).data(range)],2);
+
+%calculate average cooling in the 3 zones
+koel_average = mean([data.signal(signal_koel(1)).data(range) data.signal(signal_koel(2)).data(range) data.signal(signal_koel(3)).data(range)],2);

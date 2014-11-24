1bb88b7c040ea527ce76acc067a72e1b4f9d97de
DriesN <driesnackaerts90@hotmail.com>
2014-11-24 19:58:32 +0100

    Differential equation

---
+++ lib/costfunction.m
@@ -0,0 +1,16 @@
+function cost = costfunction(x,inputs)
+    R = vpa(x(1)); 
+    C = vpa(x(2));
+    
+    T_meas = inputs.T_meas;
+    T_amb_meas = inputs.T_amb_meas;
+    Q_solar_meas = inputs.Q_solar_meas;
+    Q_heat_meas = inputs.Q_heat_meas;
+
+    % solve differential equation
+    syms T_amb_meas T_meas Q_solar_meas Q_heat_meas
+    sys = (Q_solar_meas+Q_heat_meas-(T_meas-T_amb_meas)./R)./C
+    eq = char(sys(1));
+    T_cal = dsolve(strcat('DT_meas=',eq))
+    cost = sum((T_cal - T_meas).^2);
+end
--- systeemidentificatie/systeemidentificatie_1zone.m
+++ systeemidentificatie/systeemidentificatie_1zone.m
@@ -1,11 +1,13 @@
+addpath('../lib');
+
 %search Temperatuur
 signalname = 'Temperatuur';
-signal_temp = find_signal(data,signalname);
-temp = data.signal(signal_temp).data(range);
+signal_temp_ambient = find_signal(data,signalname);
+temp_ambient = data.signal(signal_temp_ambient).data(range);
 
 % search Toperationeel
 signalname = 'Toperationeel';
-signal_temp_ambient = find_signal(data,signalname);
+signal_temp = find_signal(data,signalname);
 
 % search Intern (invallende zonne-energie)
 signalname = 'Intern';
@@ -16,7 +18,7 @@ signalname = 'Verwarming';
 signal_verw = find_signal(data,signalname);
 
 %calculate average temp in the 3 zones
-temp_ambient_average = mean([data.signal(signal_temp_ambient(1)).data(range) data.signal(signal_temp_ambient(2)).data(range) data.signal(signal_temp_ambient(3)).data(range)],2);
+temp_average = mean([data.signal(signal_temp(1)).data(range) data.signal(signal_temp(2)).data(range) data.signal(signal_temp(3)).data(range)],2);
 
 %calculate average solarenergy in the 3 zones
 solar_average = mean([data.signal(signal_solar(1)).data(range) data.signal(signal_solar(2)).data(range) data.signal(signal_solar(3)).data(range)],2);
@@ -25,3 +27,9 @@ solar_average = mean([data.signal(signal_solar(1)).data(range) data.signal(signa
 verw_average = mean([data.signal(signal_verw(2)).data(range) data.signal(signal_verw(3)).data(range) data.signal(signal_verw(4)).data(range)],2);
 
 
+%create inputstructure
+inp = struct('T_meas',{temp_average},'T_amb_meas',{temp_ambient},'Q_solar_meas',{solar_average},'Q_heat_meas',{verw_average},'t',{data.time(range)});
+
+%optimalisation
+x0 = [3,1000000];
+x = fminsearch(@(x) costfunction(x,inp),x0,optimset('Display','iter'));

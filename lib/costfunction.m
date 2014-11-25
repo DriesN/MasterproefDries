132eb573125874bb84484d6301b0410433cac985
DriesN <driesnackaerts90@hotmail.com>
2014-11-25 13:32:39 +0100

    Aanpassing differentiaal vgl
    nog steeds error in fminsearch, maar de diff vgl wordt nu wel
    uitgerekend

--- lib/costfunction.m
+++ lib/costfunction.m
@@ -6,11 +6,10 @@ function cost = costfunction(x,inputs)
     T_amb_meas = inputs.T_amb_meas;
     Q_solar_meas = inputs.Q_solar_meas;
     Q_heat_meas = inputs.Q_heat_meas;
-
+    
     % solve differential equation
-    syms T_amb_meas T_meas Q_solar_meas Q_heat_meas
-    sys = (Q_solar_meas+Q_heat_meas-(T_meas-T_amb_meas)./R)./C
+    sys = (Q_solar_meas+Q_heat_meas-(T_meas-T_amb_meas)./R)./C;
     eq = char(sys(1));
-    T_cal = dsolve(strcat('DT_meas=',eq))
+    T_cal = dsolve(strcat('DT_meas=',eq));
     cost = sum((T_cal - T_meas).^2);
 end

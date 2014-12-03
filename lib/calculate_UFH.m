18f7f8e409e6c435a1e32a704c803bdec99cc8e7
DriesN <driesnackaerts90@hotmail.com>
2014-12-03 17:50:41 +0100

    Systeemidentificatie (1 zone) met vloerverwarming
    temp_vloer berekening a.d.h.v Q=Cv*dT
    —>calculate_UFH.m (UnderFloor Heating)

---
+++ lib/calculate_UFH.m
@@ -0,0 +1,20 @@
+function temp_floor = calculate_UFH(Q, time)
+
+    Q_heat_meas = Q;
+    t = time;
+    T(1) = 35;
+    m = 100;
+    
+    %calculate temperatuur of underfloor heating with: Q = m*c*dT
+    for i = 1:length(Q_heat_meas)-1
+        T(i+1) = (Q_heat_meas(i)./(t(i+1)-t(i)))./(m.*4186)+T(i); 
+    end
+ 
+    %rotate T matrix
+    for i = 1:3
+        T = rot90(T);
+    end
+    
+    temp_floor = T;
+end
+
--- systeemidentificatie/systeemidentificatie_1zone_metUFH.m
+++ systeemidentificatie/systeemidentificatie_1zone_metUFH.m
@@ -29,15 +29,21 @@ solar_average = mean([data.signal(signal_solar(1)).data(range) data.signal(signa
 %calculate average heating in the 3 zones
 verw_average = mean([data.signal(signal_verw(2)).data(range) data.signal(signal_verw(3)).data(range) data.signal(signal_verw(4)).data(range)],2);
 
+%calculate temperature underfloor heating
+temp_floor = calculate_UFH(verw_average, data.time(range));
+
 %create inputstructure
-inp = struct('T_meas',{temp_average},'T_amb_meas',{temp_ambient},'Q_solar_meas',{solar_average},'Q_heat_meas',{verw_average},'t',{data.time(range)});
+inp = struct('T_meas',{temp_average},'T_amb_meas',{temp_ambient},'T_floor',{temp_floor},'Q_solar_meas',{solar_average},'Q_heat_meas',{verw_average},'t',{data.time(range)});
 
 %optimalisation
 x0 = [3,1000000];
 [x,fval] = fminsearch(@(x) costfunction(x,inp),x0,optimset('Display','iter'));
+
+
 R = x(1)
 C = x(2)
 T_cal(1) = inp.T_meas(1);
+
 for i = 1:length(inp.T_meas)-1
     T_cal(i+1) = T_cal(i) + (inp.Q_solar_meas(i)-(T_cal(i)-inp.T_amb_meas(i))./R)./C .*(inp.t(i+1)-inp.t(i)); 
 end

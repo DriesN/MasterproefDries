b762867ae76daf033585aafcf2ba5e3ce6325134
DriesN <driesnackaerts90@hotmail.com>
2015-01-28 19:00:13 +0100

    Toevoegingen
    Methode met interne winsten en vloerverwarming opgedeeld in oppervlak
    en kern.

--- lib/costfunction.m
+++ lib/costfunction.m
@@ -5,6 +5,7 @@ function cost = costfunction(x,inputs,methode)
     T_meas = inputs.T_meas;
     T_amb_meas = inputs.T_amb_meas;
     Q_solar_meas = inputs.Q_solar_meas;
+    Q_intern = inputs.Q_intern;
     Q_heat = inputs.Q_heatpump.*4 + inputs.Q_gas;
     t = inputs.t;
     
@@ -25,6 +26,23 @@ function cost = costfunction(x,inputs,methode)
         for i = 1:length(T_meas)-1
             T_cal(i+1) = T_cal(i) + ((Q_solar_meas(i)+((T_floor(i)-T_cal(i))./R_v)-((T_cal(i)-T_amb_meas(i))./R))./C).*(t(i+1)-t(i));
             T_floor(i+1) = T_floor(i) + ((Q_heat(i)-((T_floor(i)-T_cal(i))./R_v))./C_v).*(t(i+1)-t(i));
+        end
+    end
+    
+    %solve differential equation (numerical) -->1 zone met Q_intern
+    %splitsing oppervlak_UFH en kern_UFH
+    if strcmp(methode, 'systeemidentificatie_1zone_metUFH_opp_kern')
+        R_v = x(3);
+        C_v = x(4);
+        R_opp = x(5);
+        C_opp = x(6);
+        T_floor = 50;
+        T_opp = 35;
+        T_cal(1) = T_meas(1);
+        for i = 1:length(T_meas)-1
+            T_cal(i+1) = T_cal(i) + ((Q_intern(i)+((T_opp(i)-T_cal(i))./R_opp)-((T_cal(i)-T_amb_meas(i))./R))./C).*(t(i+1)-t(i));
+            T_opp(i+1) = T_opp(i) + ((Q_solar_meas(i)+((T_floor(i)-T_opp(i))./R_v)-((T_opp(i)-T_cal(i))./R_opp))./C_opp).*(t(i+1)-t(i));
+            T_floor(i+1) = T_floor(i) + ((Q_heat(i)-((T_floor(i)-T_opp(i))./R_v))./C_v).*(t(i+1)-t(i));
         end
     end
     
--- systeemidentificatie/systeemidentificatie.m
+++ systeemidentificatie/systeemidentificatie.m
@@ -13,13 +13,17 @@ if strcmp(filename,'../data/knxcontrol_measurements_20140915_20141001.csv')
     signal_temp_ambient = find_signal(data,signalname);
     temp_ambient = data.signal(signal_temp_ambient).data(range);
 
-    % search Toperationeel
+    %search Toperationeel
     signalname = 'Toperationeel';
     signal_temp = find_signal(data,signalname);
 
-    % search irradiatie (invallende zonne-energie)
+    %search irradiatie (invallende zonne-energie)
     signalname = 'Irradiatie';
     signal_solar = find_signal(data,signalname);
+    
+    %search internal heatflow
+    signalname = 'Intern';
+    signal_intern = find_signal(data,signalname);
 
     %search Verwarming
     signalname = 'Verwarming';
@@ -32,9 +36,13 @@ else
     signal_temp = find_signal(data,signalname);
     temp_ambient = data.signal(signal_temp(1)).data(range);
 
-    % search irradiatie (invallende zonne-energie)
+    %search irradiatie (invallende zonne-energie)
     signalname = 'Irradiation';
     signal_solar = find_signal(data,signalname);
+    
+    %search internal heatflow
+    signalname = 'Internal';
+    signal_intern = find_signal(data,signalname);
 
     %search Verwarming
     signalname = 'elektriciteit_verwarming';
@@ -53,16 +61,19 @@ end
 %calculate average solarenergy in the 3 zones
 solar_average = mean([data.signal(signal_solar(1)).data(range) data.signal(signal_solar(2)).data(range) data.signal(signal_solar(3)).data(range)],2);
 
+%calculate average internal heatflow in the 3 zones
+intern_average = mean([data.signal(signal_intern(1)).data(range) data.signal(signal_intern(2)).data(range) data.signal(signal_intern(3)).data(range)],2); 
+
 %calculate heating
 verw_heatpump = data.signal(signal_verw_heatpump(1)).data(range);
-verw_gas = data.signal(signal_verw_gas).data(range);
+verw_gas = data.signal(signal_verw_gas).data(range).*0.8;
 
 %smooth datasignals
 temp_ambient = smooth(temp_ambient,'rlowess');
 temp_average = smooth(temp_average,'rlowess');
 
 %create inputstructure
-inp = struct('T_meas',{temp_average},'T_amb_meas',{temp_ambient},'Q_solar_meas',{solar_average},'Q_heatpump',{verw_heatpump},'Q_gas',{verw_gas},'t',{data.time(range)});
+inp = struct('T_meas',{temp_average},'T_amb_meas',{temp_ambient},'Q_solar_meas',{solar_average},'Q_heatpump',{verw_heatpump},'Q_gas',{verw_gas}, 'Q_intern',{intern_average},'t',{data.time(range)});
 
 
 %optimalisatie eerste methode
@@ -90,7 +101,7 @@ if strcmp(methode,'systeemidentificatie_1zone')
     datetick('x', 20);
 end
 
-%optimalisatie tweede methode
+%optimalisatie tweede methode (met vloerverwarming)
 if strcmp(methode,'systeemidentificatie_1zone_metUFH')
     x0 = [0.0058,8.0563e+07,3,1000000];
     [x,fval] = fminsearch(@(x) costfunction(x,inp,methode),x0,optimset('Display','iter'));
@@ -100,8 +111,49 @@ if strcmp(methode,'systeemidentificatie_1zone_metUFH')
     C_v = x(4)
 
     T_cal(1) = inp.T_meas(1);
-    for i = 1:length(inp.T_meas)-1
-        T_cal(i+1) = T_cal(i) + (inp.Q_solar_meas(i)-(T_cal(i)-inp.T_amb_meas(i))./R)./C .*(inp.t(i+1)-inp.t(i)); 
+    Q_heat = inp.Q_heatpump.*4 + inp.Q_gas;
+    T_floor(1) = 35;
+    
+    for i = 1:length(inp.T_meas)-1        
+        T_cal(i+1) = T_cal(i) + ((inp.Q_solar_meas(i)+((T_floor(i)-T_cal(i))./R_v)-((T_cal(i)-inp.T_amb_meas(i))./R))./C).*(inp.t(i+1)-inp.t(i));
+        T_floor(i+1) = T_floor(i) + ((Q_heat(i)-((T_floor(i)-T_cal(i))./R_v))./C_v).*(inp.t(i+1)-inp.t(i));
+    end
+    for i = 1:3
+        T_cal = rot90(T_cal);
+    end
+    
+    subplot(2,1,1)
+    plot(localtime(range),temp_average)
+    title 'Gemiddelde temperatuur in de 3 zones';
+    datetick('x', 20);
+    subplot(2,1,2)
+    plot(localtime(range),T_cal)
+    title 'Berekende temperatuur';
+    datetick('x', 20);
+end
+
+
+%optimalisatie derde methode (met interne winsten en vloerverwarming--> splitsing oppervlakte en kern)
+if strcmp(methode,'systeemidentificatie_1zone_metUFH_opp_kern')
+    x0 = [0.0058,8.0563e+07,3,1000000,3,1000000];
+    [x,fval] = fminsearch(@(x) costfunction(x,inp,methode),x0,optimset('Display','iter'));
+    R = x(1)
+    C = x(2)
+    R_v = x(3)
+    C_v = x(4)
+    R_opp = x(5)
+    C_opp = x(6)
+
+    T_cal(1) = inp.T_meas(1);
+    Q_heat = inp.Q_heatpump.*4 + inp.Q_gas;
+    T_floor(1) = 50;
+    T_opp(1) = 35;
+    
+    
+    for i = 1:length(inp.T_meas)-1        
+        T_cal(i+1) = T_cal(i) + ((inp.Q_intern(i)+((T_opp(i)-T_cal(i))./R_opp)-((T_cal(i)-inp.T_amb_meas(i))./R))./C).*(inp.t(i+1)-inp.t(i));
+        T_opp(i+1) = T_opp(i) + ((inp.Q_solar_meas(i)+((T_floor(i)-T_opp(i))./R_v)-((T_opp(i)-T_cal(i))./R_opp))./C_opp).*(inp.t(i+1)-inp.t(i));
+        T_floor(i+1) = T_floor(i) + ((Q_heat(i)-((T_floor(i)-T_opp(i))./R_v))./C_v).*(inp.t(i+1)-inp.t(i));
     end
     for i = 1:3
         T_cal = rot90(T_cal);

d6658e62babc3369b19cecd412ca16f082b23186
DriesN <driesnackaerts90@hotmail.com>
2015-01-28 14:12:16 +0100

    Aanpassingen woe 28 jan
    costfunction: diff vgln aangepast
    systeemidentificatie: script samengevoegd tot één script

--- lib/costfunction.m
+++ lib/costfunction.m
@@ -1,7 +1,7 @@
 function cost = costfunction(x,inputs,methode)    
     R = x(1); 
     C = x(2);
-
+    
     T_meas = inputs.T_meas;
     T_amb_meas = inputs.T_amb_meas;
     Q_solar_meas = inputs.Q_solar_meas;
@@ -12,7 +12,7 @@ function cost = costfunction(x,inputs,methode)
     if strcmp(methode, 'systeemidentificatie_1zone')
         T_cal(1)   = T_meas(1);
         for i = 1:length(T_meas)-1
-            T_cal(i+1) = T_cal(i) + (Q_solar_meas(i)+Q_heat(i)-(T_cal(i)-T_amb_meas(i))./R)./C .*(t(i+1)-t(i)); 
+            T_cal(i+1) = T_cal(i) + ((Q_solar_meas(i)+Q_heat(i)-(T_cal(i)-T_amb_meas(i))./R)./C) .*(t(i+1)-t(i)); 
         end
     end
     
@@ -20,12 +20,11 @@ function cost = costfunction(x,inputs,methode)
     if strcmp(methode, 'systeemidentificatie_1zone_metUFH')
         R_v = x(3);
         C_v = x(4);
-        T_floor = inputs.T_floor;
+        T_floor = 35;
         T_cal(1) = T_meas(1);
-        
         for i = 1:length(T_meas)-1
-            T_cal(i+1) = T_cal(i) + (Q_solar_meas(i)+Q_heat(i)-C_v.*((T_floor(i+1)-T_floor(i))./(t(i+1)-t(i)))-(T_cal(i)-T_amb_meas(i))./R)./C .*(t(i+1)-t(i));
-            Q_heat(i) = ((T_floor(i)-T_cal(i))./R_v)+C_v.*(T_floor(i+1)-T_floor(i))./(t(i+1)-t(i));
+            T_cal(i+1) = T_cal(i) + ((Q_solar_meas(i)+((T_floor(i)-T_cal(i))./R_v)-((T_cal(i)-T_amb_meas(i))./R))./C).*(t(i+1)-t(i));
+            T_floor(i+1) = T_floor(i) + ((Q_heat(i)-((T_floor(i)-T_cal(i))./R_v))./C_v).*(t(i+1)-t(i));
         end
     end
     

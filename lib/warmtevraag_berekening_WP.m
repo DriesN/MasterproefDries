e984daa1328b61b475ff8ea68902bfb596b55ce7
DriesN <driesnackaerts90@hotmail.com>
2015-05-11 10:21:12 +0200

    Aanpassingen

--- lib/warmtevraag_berekening_WP.m
+++ lib/warmtevraag_berekening_WP.m
@@ -2,9 +2,9 @@ function [warmtevraag] = warmtevraag_berekening_WP(verschil)
 
 %berekening compressorvermogen van de warmtepomp    
 if verschil < -0.5
-    warmtevraag = 1100;
+    warmtevraag = 1600;
 else
-    warmtevraag = -1100.*verschil + 550;
+    warmtevraag = -1600.*verschil + 800;
 end
 
 if verschil > 0.5
